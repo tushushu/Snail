5a90cc1972fc2d1e8e142bfff687f1e83c602cbb
tushushu <xmaseve1984@gmail.com>
2022-08-13 18:57:24 +0800

    tokens

---
+++ src/tokens.py
@@ -0,0 +1,50 @@
+from enum import Enum
+from http.client import CONTINUE
+
+
+class Tok(Enum):
+    # Types
+    INT = "int"
+    FLOAT = "float"
+    STRING = "str"
+    BOOL = "bool"
+
+    # Arithmetic operators
+    ADD = "+"
+    SUB = "-"
+    MUL = "*"
+    DIV = "/"
+
+    # Comparison operators
+    EQ = "="
+    NE = "!="
+    GT = ">"
+    GE = ">="
+    LT = "<"
+    LE = "<="
+
+    # Punctuations
+    DOT = "."
+    COMMA = ","
+    LPAR = "("
+    RPAR = ")"
+    LSQB = "["
+    RSQB = "]"
+    LB = "{"
+    RB = "}"
+    EOF = "EOF"
+
+    # Keywords(alphabetically):
+    AND = "and"
+    BREAK = "break"
+    CONTINUE = "continue"
+    ELSE = "else"
+    FALSE = "false"
+    FN = "fn"
+    FOR = "for"
+    IF = "if"
+    NOT = "not"
+    OR = "or"
+    RETURN = "return"
+    TRUE = "true"
+    WHILE = "while"
